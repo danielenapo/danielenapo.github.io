55e32373a232fadc63de664633739cb805b7a704
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2018-05-03 11:54:39 +0200

    fix touch

--- progetti/MegaRun/script.js
+++ progetti/MegaRun/script.js
@@ -196,18 +196,19 @@ function controlli(){
 		contaSprite++;
 
 	//CONTROLLO COMANDI PREMUTI
-	if(keyIsDown(UP_ARROW)|| touchX>=larghezzaPagina/2){
+	if(keyIsDown(UP_ARROW)|| mouseX>=larghezzaPagina/2){
 		if(player.onGround==true){
 			jumpfx.play();
 			player.salta();
 		}
 	}
-	if((keyIsDown(RIGHT_ARROW)&& contaSpara>rateoDiFuoco) || touchX<=larghezzaPagina/2){
+	if(keyIsDown(RIGHT_ARROW) || mouseX<=larghezzaPagina/2){
+		if(contaSpara>rateoDiFuoco){
 		var colpo= new Proiettile("img/player.png", spriteProiettile[0], spriteProiettile[1], lunghezzaProiettile, larghezzaProiettile, player.positionX+(player.width/2)-10, player.positionY+(player.height/2)-10);
 		colpo.sprites=loadImage("img/player.png");
 		colpi.push(colpo);
 		contaSpara=0;
-		shootfx.play();
+		shootfx.play();}
 	}
 
 	//CONTROLLO DIFFICOLTA' DI GIOCO
