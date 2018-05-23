5db4d000d538f89adab56d58e682cc17a4b32981
Daniele Napolitano <33985608+danielenapo@users.noreply.github.com>
2018-05-23 09:04:08 +0200

    final fix (type 1 enemy)

--- megarun/script.js
+++ megarun/script.js
@@ -266,9 +266,10 @@ function controlli(){
 			enemy.positionY= map(noise(fluttua),0,1,0,lunghezzaCanvas-130);
 		}
 		else{
-			fluttua+=0.5;
+			//fluttua+=0.5;
 			enemy.positionX-=6;
-			enemy.positionY=((Math.sin(fluttua))*30)+100; 	//se il nemico è vivo viene spostato verso sinistra e fluttua
+			//enemy.positionY=((Math.sin(fluttua))*30)+100; 	//se il nemico è vivo viene spostato verso sinistra e fluttua
+			enemy.positionY=(lunghezzaCanvas-(lunghezzaCanvas/3)), 2, 0 );	
 		}
 	}
 }
